2602bf39078e99c2ef2a984d49ae2351945f3d0b
Saksham <skwebofficials@.com>
2024-02-12 14:05:23 +0530

    First Commit

---
+++ src/Pages/Shop.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import Hero from '../Components/Hero/Hero'
+import Popular from '../Components/Popular/Popular'
+import Offers from '../Components/Offers/Offers'
+import NewCollections from '../Components/NewCollections/NewCollections'
+import NewsLetter from '../Components/NewsLetter/NewsLetter'
+import ProductDisplay from '../Components/ProductDisplay/ProductDisplay'
+
+
+const Shop = () => {
+  return (
+    <div>
+      <Hero/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <Popular/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <Offers/>
+      <NewCollections/>
+      <NewsLetter/>
+    </div>
+  )
+}
+
+export default Shop
